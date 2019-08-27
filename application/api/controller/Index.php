97d13bf2544c73e35c3a98d6411e521af27973f8
liutao <282813637@qq.com>
2019-08-27 15:00:49 +0800

    去除冗余表

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -19,14 +19,14 @@ class Index extends Api
      */
     public function index()
     {
-        // $this->success('请求成功');
-        $list = Db::view("employees_info")
-                    -> view("employees_marry","YGID,POXM,PONL,POZY","employees_info.ID = employees_marry.YGID","LEFT")
-                    -> view("employees_school","YGID,XL,ZY,BYSJ,XX,WYQK","employees_info.ID = employees_school.YGID","LEFT")
-                    -> view("employees_position","ZWMC","employees_info.ZC = employees_position.ID")
-                    -> view("employees_department","BMMC","employees_info.GZBM = employees_department.ID")
-                    -> select();
-        dump(Db::getLastSql());
-        dump($list);
+        $this->success('请求成功');
+        // $list = Db::view("employees_info")
+        //             -> view("employees_marry","YGID,POXM,PONL,POZY","employees_info.ID = employees_marry.YGID","LEFT")
+        //             -> view("employees_school","YGID,XL,ZY,BYSJ,XX,WYQK","employees_info.ID = employees_school.YGID","LEFT")
+        //             -> view("employees_position","ZWMC","employees_info.ZC = employees_position.ID")
+        //             -> view("employees_department","BMMC","employees_info.GZBM = employees_department.ID")
+        //             -> select();
+        // dump(Db::getLastSql());
+        // dump($list);
     }
 }
